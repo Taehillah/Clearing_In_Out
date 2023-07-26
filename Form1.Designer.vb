a665cd324fa3dc1b6dada968142407476875d56f
Taehillah <ishmaelmafole@gmail.com>
2023-07-26 21:09:18 +0200

    rearrangment

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,6 +22,7 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
         txtForceNum = New TextBox()
         lblForceNum = New Label()
         lblRank = New Label()
@@ -36,11 +37,15 @@ Partial Class Form1
         Label1 = New Label()
         lblReturnDate = New Label()
         dtpReturnDate = New DateTimePicker()
+        PictureBox1 = New PictureBox()
+        txtUnitVisiting = New TextBox()
+        lblUnitVisiting = New Label()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
         ' txtForceNum
         ' 
-        txtForceNum.Location = New Point(146, 124)
+        txtForceNum.Location = New Point(140, 131)
         txtForceNum.Name = "txtForceNum"
         txtForceNum.Size = New Size(176, 23)
         txtForceNum.TabIndex = 0
@@ -48,7 +53,7 @@ Partial Class Form1
         ' lblForceNum
         ' 
         lblForceNum.AutoSize = True
-        lblForceNum.Location = New Point(51, 132)
+        lblForceNum.Location = New Point(45, 139)
         lblForceNum.Name = "lblForceNum"
         lblForceNum.Size = New Size(86, 15)
         lblForceNum.TabIndex = 1
@@ -57,7 +62,7 @@ Partial Class Form1
         ' lblRank
         ' 
         lblRank.AutoSize = True
-        lblRank.Location = New Point(51, 174)
+        lblRank.Location = New Point(45, 181)
         lblRank.Name = "lblRank"
         lblRank.Size = New Size(36, 15)
         lblRank.TabIndex = 2
@@ -67,7 +72,7 @@ Partial Class Form1
         ' 
         cbRank.FormattingEnabled = True
         cbRank.Items.AddRange(New Object() {"Pte", "L/Cpl ", "Cpl ", "Sgt ", "S/Sgt", "WO2", "WO1", "SWO", "MWO", "CWO", "SCWO", "MCWO", "Chaplain", "CO", "2Lt ", "Lt ", "Capt", "Maj", "Lt Col", "Col", "Brig Gen", "Maj Gen", "Lt Gen", "Gen"})
-        cbRank.Location = New Point(146, 166)
+        cbRank.Location = New Point(140, 173)
         cbRank.Name = "cbRank"
         cbRank.Size = New Size(176, 23)
         cbRank.TabIndex = 3
@@ -76,7 +81,7 @@ Partial Class Form1
         ' lblName
         ' 
         lblName.AutoSize = True
-        lblName.Location = New Point(51, 216)
+        lblName.Location = New Point(45, 223)
         lblName.Name = "lblName"
         lblName.Size = New Size(69, 15)
         lblName.TabIndex = 4
@@ -84,7 +89,7 @@ Partial Class Form1
         ' 
         ' TextBox1
         ' 
-        TextBox1.Location = New Point(146, 208)
+        TextBox1.Location = New Point(140, 215)
         TextBox1.Name = "TextBox1"
         TextBox1.Size = New Size(176, 23)
         TextBox1.TabIndex = 5
@@ -92,7 +97,7 @@ Partial Class Form1
         ' lblHomeUnit
         ' 
         lblHomeUnit.AutoSize = True
-        lblHomeUnit.Location = New Point(51, 258)
+        lblHomeUnit.Location = New Point(45, 265)
         lblHomeUnit.Name = "lblHomeUnit"
         lblHomeUnit.Size = New Size(73, 15)
         lblHomeUnit.TabIndex = 6
@@ -100,14 +105,14 @@ Partial Class Form1
         ' 
         ' txtHomeUnit
         ' 
-        txtHomeUnit.Location = New Point(146, 250)
+        txtHomeUnit.Location = New Point(140, 257)
         txtHomeUnit.Name = "txtHomeUnit"
         txtHomeUnit.Size = New Size(176, 23)
         txtHomeUnit.TabIndex = 7
         ' 
         ' txtAuthNum
         ' 
-        txtAuthNum.Location = New Point(146, 334)
+        txtAuthNum.Location = New Point(523, 173)
         txtAuthNum.Name = "txtAuthNum"
         txtAuthNum.Size = New Size(176, 23)
         txtAuthNum.TabIndex = 8
@@ -115,7 +120,7 @@ Partial Class Form1
         ' lblAuthNum
         ' 
         lblAuthNum.AutoSize = True
-        lblAuthNum.Location = New Point(51, 342)
+        lblAuthNum.Location = New Point(428, 181)
         lblAuthNum.Name = "lblAuthNum"
         lblAuthNum.Size = New Size(79, 15)
         lblAuthNum.TabIndex = 9
@@ -125,7 +130,7 @@ Partial Class Form1
         ' 
         cbClearingInOut.FormattingEnabled = True
         cbClearingInOut.Items.AddRange(New Object() {"Out", "In"})
-        cbClearingInOut.Location = New Point(146, 292)
+        cbClearingInOut.Location = New Point(523, 131)
         cbClearingInOut.Name = "cbClearingInOut"
         cbClearingInOut.Size = New Size(176, 23)
         cbClearingInOut.TabIndex = 10
@@ -134,16 +139,16 @@ Partial Class Form1
         ' Label1
         ' 
         Label1.AutoSize = True
-        Label1.Location = New Point(51, 300)
+        Label1.Location = New Point(428, 139)
         Label1.Name = "Label1"
-        Label1.Size = New Size(89, 15)
+        Label1.Size = New Size(92, 15)
         Label1.TabIndex = 11
-        Label1.Text = "Clearing In/Out"
+        Label1.Text = "Clearing In/Out:"
         ' 
         ' lblReturnDate
         ' 
         lblReturnDate.AutoSize = True
-        lblReturnDate.Location = New Point(439, 132)
+        lblReturnDate.Location = New Point(428, 223)
         lblReturnDate.Name = "lblReturnDate"
         lblReturnDate.Size = New Size(77, 15)
         lblReturnDate.TabIndex = 12
@@ -151,16 +156,46 @@ Partial Class Form1
         ' 
         ' dtpReturnDate
         ' 
-        dtpReturnDate.Location = New Point(522, 124)
+        dtpReturnDate.Format = DateTimePickerFormat.Short
+        dtpReturnDate.Location = New Point(523, 217)
         dtpReturnDate.Name = "dtpReturnDate"
-        dtpReturnDate.Size = New Size(200, 23)
+        dtpReturnDate.Size = New Size(176, 23)
         dtpReturnDate.TabIndex = 14
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), Image)
+        PictureBox1.Location = New Point(345, 12)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(74, 69)
+        PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox1.TabIndex = 15
+        PictureBox1.TabStop = False
+        ' 
+        ' txtUnitVisiting
+        ' 
+        txtUnitVisiting.Location = New Point(523, 257)
+        txtUnitVisiting.Name = "txtUnitVisiting"
+        txtUnitVisiting.Size = New Size(176, 23)
+        txtUnitVisiting.TabIndex = 16
+        ' 
+        ' lblUnitVisiting
+        ' 
+        lblUnitVisiting.AutoSize = True
+        lblUnitVisiting.Location = New Point(428, 265)
+        lblUnitVisiting.Name = "lblUnitVisiting"
+        lblUnitVisiting.Size = New Size(79, 15)
+        lblUnitVisiting.TabIndex = 17
+        lblUnitVisiting.Text = "Unit(Visiting):"
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7.0F, 15.0F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
+        Controls.Add(lblUnitVisiting)
+        Controls.Add(txtUnitVisiting)
+        Controls.Add(PictureBox1)
         Controls.Add(dtpReturnDate)
         Controls.Add(lblReturnDate)
         Controls.Add(Label1)
@@ -177,6 +212,7 @@ Partial Class Form1
         Controls.Add(txtForceNum)
         Name = "Form1"
         Text = "Clearing In Out"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
         PerformLayout()
     End Sub
@@ -195,4 +231,7 @@ Partial Class Form1
     Friend WithEvents Label1 As Label
     Friend WithEvents lblReturnDate As Label
     Friend WithEvents dtpReturnDate As DateTimePicker
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents txtUnitVisiting As TextBox
+    Friend WithEvents lblUnitVisiting As Label
 End Class
