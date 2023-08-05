a9917a77f5bc8a8f1a7675d651eef2045db32dd0
Taehillah <ishmaelmafole@gmail.com>
2023-08-05 16:15:26 +0200

    data grid view

--- Form2.Designer.vb
+++ Form2.Designer.vb
@@ -22,18 +22,32 @@ Partial Class Form2
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        PrintDocument1 = New Printing.PrintDocument()
+        dtvResponse = New DataGridView()
+        CType(dtvResponse, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
+        ' 
+        ' dtvResponse
+        ' 
+        dtvResponse.Anchor = AnchorStyles.None
+        dtvResponse.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        dtvResponse.GridColor = SystemColors.ControlLight
+        dtvResponse.Location = New Point(166, 85)
+        dtvResponse.Name = "dtvResponse"
+        dtvResponse.RowTemplate.Height = 25
+        dtvResponse.Size = New Size(435, 266)
+        dtvResponse.TabIndex = 0
         ' 
         ' Form2
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
+        Controls.Add(dtvResponse)
         Name = "Form2"
         Text = "Form2"
+        CType(dtvResponse, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
     End Sub
 
-    Friend WithEvents PrintDocument1 As Printing.PrintDocument
+    Friend WithEvents dtvResponse As DataGridView
 End Class
